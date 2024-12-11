fce6d2db371bd33ee309b8cf18ae186b8abc2ffc
Izabelle <74457757+IzabelleVi@users.noreply.github.com>
2024-12-11 21:11:18 +0100

    Update Doublelinkedlist.cs
    Comments toe gevoegd, code verbeterd & verwijderd

--- Grote_Opdracht/Doublelinkedlist.cs
+++ Grote_Opdracht/Doublelinkedlist.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Grote_Opdracht
 {
 
-    class Node
+    class Node //Dit verwijst naar een bedrijf binnen onze doublelinked list
     {
         public Bedrijf data;
         public Node next;
@@ -20,7 +20,7 @@ namespace Grote_Opdracht
             previous = null;
         }
     }
-    class DoubleLinkedList
+    class DoubleLinkedList // Dit is de doublelinked list
     {
         public Node head;
         public Node tail;
@@ -33,12 +33,12 @@ namespace Grote_Opdracht
             count = 0;
         }
 
-        public int Count
+        public int Count //Hoeveelheid items in de list
         {
             get { return count; }
         }
 
-        public void AddFirst(Node node)
+        public void AddFirst(Node node) // Voeg een nieuw items vooraan toe
         {
             if (head == null)
             {
@@ -55,7 +55,7 @@ namespace Grote_Opdracht
             }
         }
 
-        public void AddLast(Node node)
+        public void AddLast(Node node) // Voeg een item achteraan toe
         {
             if (head == null)
             {
@@ -72,7 +72,7 @@ namespace Grote_Opdracht
             }
         }
 
-        public void RemoveFirst()
+        public void RemoveFirst() // Verwijder het eerste item
         {
             if (head == null)
             {
@@ -86,7 +86,8 @@ namespace Grote_Opdracht
             }
         }
 
-        public void RemoveLast()
+
+        public void RemoveLast() // Verwijder het laatste item
         {
             if (head == null)
             {
@@ -100,7 +101,7 @@ namespace Grote_Opdracht
             }
         }
 
-        public void Remove(Node node)
+        public void Remove(Node node) // Verwijder een gespecificeerde node (dit kost O(n) tijd)
         {
             if (head == null)
             {
@@ -125,14 +126,14 @@ namespace Grote_Opdracht
             }
         }
 
-        public void Clear()
+        public void Clear() // De list leeg maken
         {
             head = null;
             tail = null;
             count = 0;
         }
 
-        public void Print()
+        public void Print() // Print de hele list
         {
             Node current = head;
             while (current != null)
@@ -142,7 +143,7 @@ namespace Grote_Opdracht
             }
         }
 
-        public Node Find(Bedrijf data)
+        public Node Find(Bedrijf data) // Zoek een bedrijf binnen de lijst. Kost O(n) tijd
         {
             Node current = head;
             while (current != null)
@@ -156,7 +157,7 @@ namespace Grote_Opdracht
         return null;
         }
 
-        public Node Index(int index)
+        public Node Index(int index) //returned het bedrijf dat op de gegeven index in de lijst zit. Kost O(n) tijd.
         {
             int i = 0;
             Node current = head;
@@ -172,45 +173,14 @@ namespace Grote_Opdracht
             return null;
         }
 
-        public void Insert(int index, Node node)
-        {
-            int i = 0;
-            Node current = head;
-            if (index == 0)
-            {
-                AddFirst(node);
-            }
-            else if (index == count)
-            {
-                AddLast(node);
-            }
-            else
-            {
-                while (current != null)
-                {
-                    if (i == index)
-                    {
-                        node.next = current;
-                        node.previous = current.previous;
-                        current.previous.next = node;
-                        current.previous = node;
-                        count++;
-                        break;
-                    }
-                    current = current.next;
-                    i++;
-                }
-            }
-            
-        }
 
-        public void VerplaatsBedrijf(Node node, int index)
+        public void VerplaatsBedrijf(Node node, int index) // Verwijderd en herplaatst het bedrijf in een gegeven index. Kost O(n) tijd.
         {
             Remove(node);
-            Insert(index, node);
+            InsertAtIndex(node, index);
         }
         
-        public void FindAndReplace(Bedrijf data, Bedrijf newData)
+        public void FindAndReplace(Bedrijf data, Bedrijf newData) // Verwijderd een bedrijf en zet op die plek in de index een nieuw, gegeven bedrijf.
         {
             Node current = head;
             while (current != null)
@@ -224,14 +194,39 @@ namespace Grote_Opdracht
             }
         }
 
-        public void WisselBedrijven(Node node1, Node node2)
+        public void WisselBedrijven(Node node1, Node node2) // Wissel 2 gegeven bedrijven binnen de list.
         {
-            Bedrijf temp = node1.data;
-            node1.data = node2.data;
-            node2.data = temp;
+            if (node1 == node2) return; // Geen actie nodig als het dezelfde node is
+
+            // Houd referenties bij voor omliggende nodes
+            Node prev1 = node1.previous;
+            Node next1 = node1.next;
+            Node prev2 = node2.previous;
+            Node next2 = node2.next;
+
+            // Update de omliggende nodes van node1
+            if (prev1 != null) prev1.next = node2;
+            if (next1 != null) next1.previous = node2;
+
+            // Update de omliggende nodes van node2
+            if (prev2 != null) prev2.next = node1;
+            if (next2 != null) next2.previous = node1;
+
+            // Wissel de previous en next van node1 en node2
+            node1.previous = prev2;
+            node1.next = next2;
+            node2.previous = prev1;
+            node2.next = next1;
+
+            // Update head en tail indien nodig
+            if (node1 == head) head = node2;
+            else if (node2 == head) head = node1;
+
+            if (node1 == tail) tail = node2;
+            else if (node2 == tail) tail = node1;
         }
 
-        public void AddList(DoubleLinkedList extraRit)
+        public void AddList(DoubleLinkedList extraRit) // Moet worden verwijderd
         {
             Node node = extraRit.head;
             while(node != extraRit.tail)
@@ -244,32 +239,38 @@ namespace Grote_Opdracht
             count++;
         }
 
-        public void InsertAtIndex(Node newNode, int index)
+        public void InsertAtIndex(Node node, int index) //Voegt bedrijf toe in gegeven index. Kost O(n/2)
         {
-            if (index == 0)
-            {
-                AddFirst(newNode);
-            }
-            else if (index == Count)
-            {
-                AddLast(newNode);
-            }
+            if (index < 0 || index > count) throw new ArgumentOutOfRangeException();
+
+            if (index == 0) AddFirst(node);
+            else if (index == count) AddLast(node);
             else
             {
-                // Voeg toe in het midden van de lijst
-                Node current = head;
-                for (int i = 0; i < index - 1; i++)
-                    current = current.next;
+                Node current;
+                if (index < count / 2)
+                {
+                    current = head;
+                    for (int i = 0; i < index - 1; i++)
+                        current = current.next;
+                }
+                else
+                {
+                    current = tail;
+                    for (int i = count - 1; i > index; i--)
+                        current = current.previous;
+                }
 
-                newNode.next = current.next;
-                newNode.previous = current;
+                node.next = current.next;
+                node.previous = current;
 
-                current.next.previous = newNode;
-                current.next = newNode;
+                current.next.previous = node;
+                current.next = node;
+                count++;
             }
         }
 
-        public void RemoveAtIndex(int index)
+        public void RemoveAtIndex(int index) // Verwijderd bedrijf van gegeven Index 
         {
             if (index == 0)
             {
@@ -292,4 +293,5 @@ namespace Grote_Opdracht
     }
             
 }
+
 
